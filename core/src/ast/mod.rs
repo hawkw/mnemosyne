3a2aeddef026b1d2b7c14bb61f5aeaf1cff42641
Hawk Weisman <hi@hawkweisman.me>
2015-08-27 14:54:05 -0400

    Start on AST

--- core/src/ast/mod.rs
+++ core/src/ast/mod.rs
@@ -1,7 +1,26 @@
-pub trait ASTNode {
+use std::fmt;
 
+pub trait ASTNode {
+    /// Pretty-print this node at the desired indent level
+    fn print_level(&self, level: usize) -> String;
 }
 
-pub enum Expression {
+impl fmt::Debug for ASTNode {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.print_level(0))
+    }
+}
 
+#[derive(PartialEq, Clone, Debug)]
+pub enum Expression {
+    // Def(DefNode),
+    // If(IfNode),
+    // Let(LetNode),
+    // Call(CallNode)
+}
+
+impl ASTNode for Expression {
+    fn print_level(&self, level: usize) -> String {
+        unimplemented!()
+    }
 }
