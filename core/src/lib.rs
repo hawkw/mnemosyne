837a2215b8adbfd8c23e5ebac7ada5d9b481c108
Hawk Weisman <hi@hawkweisman.me>
2015-08-28 12:05:06 -0400

    Versioning takes place now

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -10,3 +10,16 @@ extern crate seax_util as seax;
 pub mod position;
 pub mod ast;
 pub mod compile;
+
+use rustc::lib::llvm::{LLVMVersionMajor, LLVMVersionMinor}
+
+const VERSION_MAJOR: u32 = 0;
+const VERSION_MINOR: u32 = 1;
+
+pub fn llvm_version() -> String {
+    format!("LLVM v{}.{}", LLVMVersionMajor(), LLVMVersionMinor())
+}
+
+pub fn mnemosyne_version() -> String {
+    format!("Mnemosyne v{}.{}", VERSION_MAJOR, VERSION_MINOR)
+}
