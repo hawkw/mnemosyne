e13dbcf579a02a853ecff396f5bdce0c05cc493a
Hawk Weisman <hi@hawkweisman.me>
2015-08-28 12:37:19 -0400

    Start on compiler command-line interface

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -5,7 +5,7 @@
 // Mnemosyne is released under the MIT License. Please refer to
 // the LICENSE file at the top-level directory of this distribution
 // or at https://github.com/hawkw/mnemosyne/.
-// 
+//
 #![crate_name = "mnemosyne"]
 #![crate_type = "lib"]
 #![feature(rustc_private)]
@@ -19,13 +19,13 @@ pub mod position;
 pub mod ast;
 pub mod compile;
 
-use rustc::lib::llvm::{LLVMVersionMajor, LLVMVersionMinor}
+use rustc::lib::llvm::{LLVMVersionMajor, LLVMVersionMinor};
 
 const VERSION_MAJOR: u32 = 0;
 const VERSION_MINOR: u32 = 1;
 
 pub fn llvm_version() -> String {
-    format!("LLVM v{}.{}", LLVMVersionMajor(), LLVMVersionMinor())
+    unsafe { format!("LLVM v{}.{}", LLVMVersionMajor(), LLVMVersionMinor()) }
 }
 
 pub fn mnemosyne_version() -> String {
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+//
+// The Manganese Mnemosyne Compilation System
+// (c) 2015 Hawk Weisman
+//
+// Mnemosyne is released under the MIT License. Please refer to
+// the LICENSE file at the top-level directory of this distribution
+// or at https://github.com/hawkw/mnemosyne/.
+//
+extern crate clap;
+extern crate mnemosyne;
+
+use clap::{Arg, App, SubCommand};
+
+const VERSION_MAJOR: u32 = 0;
+const VERSION_MINOR: u32 = 1;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Manganese")
+        .version(&format!("v{}.{} for {} ({})"
+                , VERSION_MAJOR
+                , VERSION_MINOR
+                , mnemosyne::mnemosyne_version()
+                , mnemosyne::llvm_version()
+            ))
+        .author("Hawk Weisman <hi@hawkweisman.me>")
+        .about("[Mn] Manganese: The Mnemosyne Compilation System")
+        .args_from_usage(
+            "<INPUT> 'Source code file to compile'
+             -d, --debug 'Display debugging information'")
+        .get_matches();
 }
