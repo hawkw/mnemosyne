c41b083726427a4437ebce93dfe411e4059d0690
Hawk Weisman <hi@hawkweisman.me>
2015-08-30 12:48:57 -0400

    Begin on type structures

--- core/src/semantic/types.rs
+++ core/src/semantic/types.rs
@@ -2,7 +2,25 @@ use super::ASTNode;
 use seax::compiler_tools::ForkTable;
 
 pub struct Scoped<'a, T> where T: ASTNode {
-    node: T, scope: ForkTable<'a, String, Annotation>
+    node: T, scope: ForkTable<'a, String, Type>
 }
 
-pub struct Annotation;
+#[derive(Debug, Clone)]
+pub enum Type { Ref(Reference)
+              , Prim(Primitive)
+              , Algebraic //TODO: make me a thing
+              }
+
+/// Reference types
+///
+/// TODO: how will lifetime analysis actually work?
+#[derive(Debug, Clone)]
+pub enum Reference { Borrowed(Box<Type>) // TODO: borrowed from where?
+                   , Moved(Box<Type>) // TODO: moved from where?
+                   , Owned(Box<Type>)
+                   , Raw(Box<Type>)
+                   }
+
+#[derive(Debug, Clone, Copy)]
+pub enum Primitive { Int, Uint, Byte, Str // TODO: finish
+}
