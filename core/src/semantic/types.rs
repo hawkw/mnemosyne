661408d5257c1dc81337ee05f89ed86b2c9b09e6
Hawk Weisman <hi@hawkweisman.me>
2015-08-31 12:13:51 -0400

    Apparently the rustdoc parser hates Haskell comma style. Oops

--- core/src/semantic/types.rs
+++ core/src/semantic/types.rs
@@ -35,47 +35,49 @@ impl<'a, T> Annotated<'a, T> where T: ASTNode {
 }
 
 #[derive(Debug, Clone)]
-pub enum Type { /// Reference types
-                Ref(Reference)
-                /// Primitive types
-              , Prim(Primitive)
-                /// An algebraic data type.
-                ///
-                /// Represented as a vector of variants.
-              , Algebraic(Vec<Type>)
-                /// A function.
-                ///
-                /// Represented as a vector of parameters and a return type.
-              , Function { params: Vec<Type>
-                         , rt: Rc<Type> }
-                /// A unique symbol type (`'symbol` syntax)
-              , Symbol(String)
-              }
+pub enum Type {
+    /// Reference types
+    Ref(Reference),
+    /// Primitive types
+    Prim(Primitive),
+    /// An algebraic data type.
+    ///
+    /// Represented as a vector of variants.
+    Algebraic(Vec<Type>),
+    /// A function.
+    ///
+    /// Represented as a vector of parameters and a return type.
+    Function { params: Vec<Type>
+             , rt: Rc<Type> },
+    /// A unique symbol type (`'symbol` syntax)
+    Symbol(String)
+}
 
 /// Reference types (pointers)
 ///
 /// TODO: how will lifetime analysis actually work?
 #[derive(Debug, Clone)]
-pub enum Reference { /// A reference borrowed from another scope.
-                     ///
-                     /// Semantically similar to Rust's `&`-pointers.
-                     ///
-                     /// TODO: should this track where it was borrowed from?
-                     /// (can we even perform this analysis at this stage?)
-                     Borrowed(Rc<Type>)
-                     /// A moved reference from another scope
-                     ///
-                     /// TODO: should this track where it was moved from?
-                     /// (can we even perform this analysis at this stage?)
-                   , Moved(Rc<Type>)
-                     /// A unique (i.e. boxed) reference.
-                   , Unique(Rc<Type>)
-                     /// A raw (unsafe) reference.
-                     ///
-                     /// Unfortunately we have to have this because of
-                     /// reasons (i.e, FFI).
-                   , Raw(Rc<Type>)
-                   }
+pub enum Reference {
+    /// A reference borrowed from another scope.
+    ///
+    /// Semantically similar to Rust's `&`-pointers.
+    ///
+    /// TODO: should this track where it was borrowed from?
+    /// (can we even perform this analysis at this stage?)
+    Borrowed(Rc<Type>),
+    /// A moved reference from another scope
+    ///
+    /// TODO: should this track where it was moved from?
+    /// (can we even perform this analysis at this stage?)
+    Moved(Rc<Type>),
+    /// A unique (i.e. boxed) reference.
+    Unique(Rc<Type>),
+    /// A raw (unsafe) reference.
+    ///
+    /// Unfortunately we have to have this because of
+    /// reasons (i.e, FFI).
+    Raw(Rc<Type>)
+}
 
 /// Language primitive types
 ///
