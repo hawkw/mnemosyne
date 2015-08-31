73599f077bd36ff00a29ac0e5b6bd4d09f84b1e6
Hawk Weisman <hi@hawkweisman.me>
2015-08-31 12:09:16 -0400

    Start on type to LLVM IR translation

--- core/src/compile/mod.rs
+++ core/src/compile/mod.rs
@@ -6,18 +6,26 @@ use rustc::lib::llvm::{ ContextRef
                       , ModuleRef
                       , ValueRef
                       , BuilderRef
+                      , TypeRef
                   };
 use seax::compiler_tools::ForkTable;
 
 use position::Positional;
 use ast::{Form, DefForm};
+use semantic::types::{Type, Reference, Primitive};
 
 pub type IRResult = Result<ValueRef, Positional<String>>;
 pub type SymbolTable<'a> = ForkTable<'a, &'a str, ValueRef>;
 
+const WORD_SIZE: c_uint = std::mem::size_of::<isize>();
+
 /// Trait for that which may join in The Great Work
 pub trait Compile {
     fn to_ir(&self, context: LLVMContext) -> IRResult;
+}
+
+pub trait TranslateType {
+    fn translate_type(&self, context: LLVMContext) -> TypeRef;
 }
 
 /// LLVM compilation context.
@@ -26,12 +34,11 @@ pub trait Compile {
 /// the [iron-kaleidoscope](https://github.com/jauhien/iron-kaleidoscope)
 /// tutorial, and from [`librustc_trans`](https://github.com/rust-lang/rust/blob/master/src/librustc_trans/trans/mod.rs)
 /// from the Rust compiler.
-pub struct LLVMContext<'a> {
-    pub llctx: ContextRef,
-    pub llmod: ModuleRef,
-    pub llbuilder: BuilderRef,
-    pub names: SymbolTable<'a>
-}
+pub struct LLVMContext<'a> { pub llctx: ContextRef
+                           , pub llmod: ModuleRef
+                           , pub llbuilder: BuilderRef
+                           , pub names: SymbolTable<'a>
+                           }
 
 // because we are in the Raw Pointer Sadness Zone (read: unsafe),
 // it is necessary that we assert that everything exists.
@@ -104,6 +111,38 @@ impl Compile for DefForm {
                         unimplemented!()
                     }
                 }
+        }
+    }
+}
+
+impl TranslateType for Type {
+    fn translate_type(&self, context: LLVMContext) -> TypeRef {
+        match *self {
+            Type::Ref(ref reference)  => reference.TranslateType(context),
+            Type::Prim(ref primitive) => primitive.translate_type(context),
+            _ => unimplemented!() // TODO: figure this out
+        }
+    }
+}
+
+impl TranslateType for Reference {
+    fn translate_type(&self, context: LLVMContext) -> TypeRef {
+        unimplemented!() // TODO: figure this out
+    }
+}
+
+impl TranslateType for Primitive {
+    fn translate_type(&self, context: LLVMContext) -> TypeRef {
+        match *self {
+            Primitive::Int => // Integers are machine word size
+                llvm::LLVMIntTypeInContext(context, WORD_SIZE),
+            Primitive::Float => // Floats are single precision
+                llvm::LLVMGetFloatTypeInContext(context),
+            Primitive::Double => // Doubles are obvious precision
+                llvm::LLVMGetDoubleTypeInContext(context),
+            Primitive::Byte => // Bytes are 8 bits (duh)
+                llvm::LLVMGetInt8TypeInContext(context)
+            _ => unimplemented!() // TODO: figure this out
         }
     }
 }
--- core/src/semantic/types.rs
+++ core/src/semantic/types.rs
@@ -35,24 +35,53 @@ impl<'a, T> Annotated<'a, T> where T: ASTNode {
 }
 
 #[derive(Debug, Clone)]
-pub enum Type { Ref(Reference)
+pub enum Type { /// Reference types
+                Ref(Reference)
+                /// Primitive types
               , Prim(Primitive)
-              , Algebraic //TODO: make me a thing
+                /// An algebraic data type.
+                ///
+                /// Represented as a vector of variants.
+              , Algebraic(Vec<Type>)
+                /// A function.
+                ///
+                /// Represented as a vector of parameters and a return type.
               , Function { params: Vec<Type>
                          , rt: Rc<Type> }
+                /// A unique symbol type (`'symbol` syntax)
               , Symbol(String)
               }
 
-/// Reference types
+/// Reference types (pointers)
 ///
 /// TODO: how will lifetime analysis actually work?
 #[derive(Debug, Clone)]
-pub enum Reference { Borrowed(Rc<Type>) // TODO: borrowed from where?
-                   , Moved(Rc<Type>) // TODO: moved from where?
-                   , Owned(Rc<Type>)
+pub enum Reference { /// A reference borrowed from another scope.
+                     ///
+                     /// Semantically similar to Rust's `&`-pointers.
+                     ///
+                     /// TODO: should this track where it was borrowed from?
+                     /// (can we even perform this analysis at this stage?)
+                     Borrowed(Rc<Type>)
+                     /// A moved reference from another scope
+                     ///
+                     /// TODO: should this track where it was moved from?
+                     /// (can we even perform this analysis at this stage?)
+                   , Moved(Rc<Type>)
+                     /// A unique (i.e. boxed) reference.
+                   , Unique(Rc<Type>)
+                     /// A raw (unsafe) reference.
+                     ///
+                     /// Unfortunately we have to have this because of
+                     /// reasons (i.e, FFI).
                    , Raw(Rc<Type>)
                    }
 
+/// Language primitive types
+///
+/// TODO: add some form of provable-refinement (i.e. we know that some value
+/// is not just a bool at compile time but that it's true/false, or we know
+/// some value is not just a number but the number 1382, or whatever).
 #[derive(Debug, Clone, Copy)]
 pub enum Primitive { Int
                    , Uint
@@ -60,5 +89,6 @@ pub enum Primitive { Int
                    , Char
                    , Str
                    , Bool
+                   , Float
                    // TODO: finish
                    }
