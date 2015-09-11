7170c36e3bbc6cb3e062b1329676238cec7b815a
Hawk Weisman <hi@hawkweisman.me>
2015-09-11 12:07:07 -0400

    Start on parser stuff

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -1,3 +1,20 @@
+extern crate combine;
+extern crate combine_language;
+use combine::*;
+use combine::primitives::Stream;
+use combine_language::{LanguageEnv, LanguageDef, Identifier};
+
+type ParseFn<'a, I, T> = fn (&LanguageEnv<'a, I>, State<I>)
+                            -> ParseResult<T, I>;
+
+struct LangParser<'a: 'b, 'b, I, T>
+where I: Stream<Item=char>
+    , I: 'b
+{
+    env: &'b LanguageEnv<'a, I>
+  , parser: ParseFn<'a, I, T>
+}
+
 #[test]
 fn it_works() {
 }
