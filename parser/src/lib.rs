5b8eca4ebc5f75d41e60cb5662fd56654f022202
Hawk Weisman <hi@hawkweisman.me>
2015-09-17 21:00:33 -0400

    Main expr parser actually happens now

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -322,14 +322,16 @@ where I: Stream<Item=char>
     }
 
 }
-pub fn parse_module<N: ?Sized>(code: &str) -> Result<Vec<N>, ParseError<&str>>
-where N: ASTNode + Sized {
+pub fn parse_module<'a>(code: &'a str)
+                        -> Result< Vec<Expr<'a, UnscopedState>>
+                                 , ParseError<&str>>
+ {
     let alpha_ext = "+-*/<=>!?:$%_~^";
     let ops = "+-*/|=<>";
     let env = LanguageEnv::new(LanguageDef {
         ident: Identifier {
-            start: letter().or(satisfy(|c| alpha_ext.contains(c)))
-          , rest: alpha_num().or(satisfy(|c| alpha_ext.contains(c)))
+            start: letter().or(satisfy(move |c| alpha_ext.contains(c)))
+          , rest: alpha_num().or(satisfy(move |c| alpha_ext.contains(c)))
           , reserved: [ "and"               , "begin"
                       , "case"              , "cond"
                       , "data"              , "define"
@@ -339,8 +341,7 @@ where N: ASTNode + Sized {
                       , "let"               , "let*"        , "letrec"
                       , "or"
                       , "quasiquote"        , "quote"       , "unquote"
-                      , "set!"
-                      , "unquote-splicing"
+                      , "set!"              , "unquote-splicing"
                       , "i8"                , "u8"
                       , "i16"               , "u16"
                       , "i32"               , "u32"         , "f32"
@@ -351,8 +352,8 @@ where N: ASTNode + Sized {
                        .collect()
         }
       , op: Identifier {
-            start: satisfy(|c| ops.contains(c))
-          , rest:  satisfy(|c| ops.contains(c))
+            start: satisfy(move |c| ops.contains(c))
+          , rest:  satisfy(move |c| ops.contains(c))
           , reserved: [ "=>", "->", "\\", "|"].iter().map(|x| (*x).into())
                                                      .collect()
         }
@@ -363,7 +364,7 @@ where N: ASTNode + Sized {
     let env = MnEnv { env: env };
 
     env.white_space()
-       .parse(code);
-
-    unimplemented!()
+       .with(many1::<Vec<Expr<'a, U>>, _>(env.expr()))
+       .parse(code)
+       .map(|(e, _)| e)
 }
