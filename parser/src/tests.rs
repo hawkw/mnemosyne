d80d33fb5b8b6a3eb0debd6e08e8bd6ec2c24297
Hawk Weisman <hi@hawkweisman.me>
2015-09-24 14:55:54 -0400

    Add parser tests for calls involving pointers

--- parser/src/tests.rs
+++ parser/src/tests.rs
@@ -21,8 +21,18 @@ expr_test!(test_basic_mul, "(* 1 2)");
 expr_test!(test_nested_arith_1, "(+ 1 (- 2 3))");
 expr_test!(test_nested_arith_2, "(* (+ 1 2) 3 4)");
 expr_test!(test_nested_arith_3, "(+ (/ 1 2) (* 3 4))");
+
 expr_test!(test_call_1, "(my_fn 1 2)");
 expr_test!(test_call_2, "(my_fn (my_other_fn a_var a_different_var))");
 expr_test!(test_call_3,
     "(my_fn (my_other_fn a_var a_different_var) VarWithUppercase Othervar)");
 expr_test!(test_call_4, "(my_fn (my_other_fn a_var a_different_var) (another_fn a_var))");
+
+expr_test!(test_call_ptr_1, "(my_fn a *b)");
+expr_test!(test_call_ptr_2, "(my_fn *a *b)");
+expr_test!(test_call_ptr_3, "(my_fn &a)");
+expr_test!(test_call_ptr_4, "(my_fn a &b)");
+expr_test!(test_call_ptr_5, "(my_fn &a &b)");
+expr_test!(test_call_ptr_6, "(my_fn @a)");
+expr_test!(test_call_ptr_7, "(my_fn a @b)");
+expr_test!(test_call_ptr_8, "(my_fn @a @b)");
