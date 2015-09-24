a0103fe1212ef1b01cbc1e4d68bc893b43d867cc
Hawk Weisman <hi@hawkweisman.me>
2015-09-24 14:23:56 -0400

    Starting on parser unit testing

--- parser/src/tests.rs
+++ parser/src/tests.rs
@@ -15,3 +15,9 @@ macro_rules! expr_test {
 }
 
 expr_test!(test_basic_add, "(+ 1 2)");
+expr_test!(test_basic_sub, "(- 3 4)");
+expr_test!(test_basic_div, "(/ 5 6)");
+expr_test!(test_basic_mul, "(* 1 2)");
+expr_test!(test_nested_arith_1, "(+ 1 (- 2 3))");
+expr_test!(test_nested_arith_2, "(* (+ 1 2) 3 4)");
+expr_test!(test_nested_arith_3, "(+ (/ 1 2) (* 3 4))");
